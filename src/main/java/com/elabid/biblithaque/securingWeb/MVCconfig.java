d08cb5b139c85dcd851c7f620e45350a5be7bf87
cheick18 <97468180+cheick18@users.noreply.github.com>
2023-12-14 03:30:34 +0100

    teste de spring securité

---
+++ src/main/java/com/elabid/biblithaque/securingWeb/MVCconfig.java
@@ -0,0 +1,16 @@
+package com.elabid.biblithaque.securingWeb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+
+public class MVCconfig implements WebMvcConfigurer{
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
---
+++ src/test/java/com/elabid/biblithaque/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elabid.biblithaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
